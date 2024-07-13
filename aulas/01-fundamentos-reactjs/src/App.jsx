8e45f17549a19f78a1de1c751bc3457fd2b0e340
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-12 22:01:40 -0300

    Componentes, propriedades e CSS modules

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,20 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 export function App() {
 
   return (
-    <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </>
+      <div>
+        <Header />
+
+        <Post 
+          author="Deyvid Matheus" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis ut voluptatem, ab quibusdam dolorum molestias iusto sed laudantium modi facere dolores aliquid quisquam provident officia error fugiat laboriosam possimus?" 
+        />
+
+        <Post 
+          author="João Silva" 
+          content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa voluptas commodi quo nihil molestiae consequatur expedita? Quisquam quod, harum quos voluptas delectus cumque facilis perspiciatis." 
+        />
+      </div>
   )
 }
