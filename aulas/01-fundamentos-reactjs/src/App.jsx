cf1d7696bd3206c67e4c3defb93ba81094ce1765
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-14 20:13:59 -0300

    Header

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,21 @@
-import { Header } from './components/Header'
+import { Header } from './components/Header';
 import { Post } from './Post'
 
-import './global.css'
+import './global.css';
 
 export function App() {
   return (
-      <div>
-        <Header />
+    <div>
+      <Header />
 
-        <Post 
-          author="Deyvid Matheus" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis ut voluptatem, ab quibusdam dolorum molestias iusto sed laudantium modi facere dolores aliquid quisquam provident officia error fugiat laboriosam possimus?" 
-        />
-
-        <Post 
-          author="João Silva" 
-          content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa voluptas commodi quo nihil molestiae consequatur expedita? Quisquam quod, harum quos voluptas delectus cumque facilis perspiciatis." 
-        />
-      </div>
+      <Post
+        author="Deyvid Matheus"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
+    </div>
   )
 }
