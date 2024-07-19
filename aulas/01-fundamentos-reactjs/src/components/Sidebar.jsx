7ed296e37f98c4e4cce25d9629ec9d93bcc67f5f
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-19 19:52:53 -0300

    Siderbar update

--- aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,24 +1,28 @@
-import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            
-            <img 
-                className={styles.cover} 
-                src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
 
-            <div className={styles.profile}>
-                <strong>Deyvid Matheus</strong>
-                <span>Web Developer</span>
-                
-                <footer>
-                    <a href="#">
-                        Editar seu perfil
-                    </a>
-                </footer>
-            </div>
-        </aside>
-    )
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/dwyvid1.png" />
+
+        <strong>Deyvid Matheus</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
